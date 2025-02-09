cb4e46a63880d0dfd446b5269492fc0f6f06a34a
RobertSajdak <robert.sajdak@onet.eu>
2025-02-09 21:40:17 +0100

    added Player component implementation for changing username

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,26 @@
+import { useState } from 'react';
+
+// Komponent przechowujący nazwę gracza
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+	const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+	const [submited, setSubmitted] = useState(false);
+
+	function handleChange(event) {
+		setSubmitted(false);
+		setEnteredPlayerName(event.target.value);
+	}
+
+	function handleClick() {
+		setSubmitted(true);
+	}
+
+	return (
+		<section id="player">
+			<h2>Welcome {submited ? enteredPlayerName : 'unknown entity'}</h2>
+			<p>
+				<input type="text" onChange={handleChange} value={enteredPlayerName} />
+				<button onClick={handleClick}>Set Name</button>
+			</p>
+		</section>
+	);
 }

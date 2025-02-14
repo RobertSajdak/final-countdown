8d9664f148ef89d45033b2f8f6f4655e20146c0c
RobertSajdak <robert.sajdak@onet.eu>
2025-02-14 20:38:56 +0100

    the pattern was used with the useImperativeHandle hook

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,28 +1,38 @@
-export default function ResultModal({ ref, result, targetTime }) {
-	return (
-		<dialog ref={ref} className="result-modal">
-			<h2>You {result}!</h2>
-			<p>
-				The target time was <strong>{targetTime} seconds.</strong>
-			</p>
-			<p>
-				You stopped the timer with <strong>X seconds left.</strong>
-			</p>
-			<form method="dialog">
-				<button>Close</button>
-			</form>
-		</dialog>
-	);
-}
+// export default function ResultModal({ ref, result, targetTime }) {
+// 	return (
+// 		<dialog ref={ref} className="result-modal">
+// 			<h2>You {result}!</h2>
+// 			<p>
+// 				The target time was <strong>{targetTime} seconds.</strong>
+// 			</p>
+// 			<p>
+// 				You stopped the timer with <strong>X seconds left.</strong>
+// 			</p>
+// 			<form method="dialog">
+// 				<button>Close</button>
+// 			</form>
+// 		</dialog>
+// 	);
+// }
 
-/**  -------------------------------------------------------------------------------------------
+
 // Dla starszych wersji React, tj. poniżej wersji 19, z użyciem funkcji wbudowanej forwardRef().
 
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+	const dialog = useRef();
+
+	useImperativeHandle(ref, () => {
+		return {
+			open() {
+				dialog.current.showModal();
+			}
+		};
+	});
+
 	return (
-		<dialog ref={ref} className="result-modal">
+		<dialog ref={dialog} className="result-modal">
 			<h2>You {result}!</h2>
 			<p>
 				The target time was <strong>{targetTime} seconds.</strong>
@@ -38,4 +48,3 @@ const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref)
 });
 
 export default ResultModal;
-*/
